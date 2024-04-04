c45341a02ba71e9cc92d24fed867c80e1d43d625
KlvnWr <kelvin.wartanabe@gmail.com>
2024-04-04 13:20:38 -0300

    Deu ruim no Hoppscocth

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import { z } from "zod"
 import { prisma } from "../../lib/prisma"
 import { FastifyInstance } from "fastify"
 import { randomUUID } from "node:crypto"
+import { redis } from "../../lib/redis"
 
 export async function voteOnPoll(app: FastifyInstance){
     app.post("/polls/:pollId/votes", async (request, reply) => {
@@ -35,6 +36,8 @@ export async function voteOnPoll(app: FastifyInstance){
                         id: userPreviousVoteOnPoll.id,
                     }
                 })
+
+                await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
             } else if(userPreviousVoteOnPoll){  // Se tiver ja votado no mesmo
                 return reply.status(400).send({ messege: "You already voted on this poll!!!"})
             }
@@ -60,6 +63,8 @@ export async function voteOnPoll(app: FastifyInstance){
                 pollOptionId,
             }
         })
+
+        await redis.zincrby(pollId, 1, pollOptionId)
         
 
         return reply.status(201).send()
