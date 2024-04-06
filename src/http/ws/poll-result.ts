aa7d6afc2f0e3932fbce7b075e1289e83354653c
KlvnWr <kelvin.wartanabe@gmail.com>
2024-04-06 02:56:21 -0300

    A versao nao funciona

--- src/http/ws/poll-result.ts
+++ src/http/ws/poll-result.ts
@@ -1,9 +1,10 @@
 import { FastifyInstance } from "fastify";
 
 export async function pollResults(app: FastifyInstance){
-    app.get('/polls/:pollId/results', { websocket: true }, (connection, request) => {
-        connection.socket.on('message', (message: string) => {
+    app.get('/polls/:pollId/results', { websocket: true }, async(connection) => {
+            console.log(connection)
+            connection.socket.on('message', (message: string) => {
             connection.socket.send('you sent:' + message)
-          })
+           })
     })
 }
