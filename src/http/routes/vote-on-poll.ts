f3f2e6e57f61611050808197eb876208825b21e7
KlvnWr <kelvin.wartanabe@gmail.com>
2024-04-07 16:48:23 -0300

    vai assim memo

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -3,6 +3,7 @@ import { prisma } from "../../lib/prisma"
 import { FastifyInstance } from "fastify"
 import { randomUUID } from "node:crypto"
 import { redis } from "../../lib/redis"
+import { voting } from "../../utils/voting-pub-sub"
 
 export async function voteOnPoll(app: FastifyInstance){
     app.post("/polls/:pollId/votes", async (request, reply) => {
@@ -65,6 +66,11 @@ export async function voteOnPoll(app: FastifyInstance){
         })
 
         await redis.zincrby(pollId, 1, pollOptionId)
+
+        voting.publish(pollId,{
+            pollOptionId,
+            votes: 1,
+        })
         
 
         return reply.status(201).send()
